f14152ccf6df9316057d0c9fea201d7f2b47cb9a
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-06 20:49:55 +0200

    change gallery

--- src/Actor.js
+++ src/Actor.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react"
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
-export default class Actor extends Component{
+export default class Actor extends Component {
     constructor(props) {
-        super (props);
+        super(props);
     }
-    render(){
-        return(<div>
-            <Card>
-                    <Card.Img  src={this.props.actor.Image} />
+    render() {
+        return (
+            <div>
+                <Card>
+                    <Card.Img src={this.props.actor.Image} />
                     <Card.Body>
-                     {/*  <Card.Title>{ this.props.actor.FirstName} {this.props.actor.LastName}</Card.Title> */}
+                        <Card.Title>{this.props.actor.FirstName} {this.props.actor.LastName}</Card.Title>
                     </Card.Body>
                 </Card>
-        </div>)
+            </div>
+        )
     }
 }
