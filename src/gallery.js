3a929d337f19b0f4c1cc5c9d5a86701b239715da
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-08 17:49:17 +0200

    search First and Last names is and header and search margin

--- src/gallery.js
+++ src/gallery.js
@@ -3,7 +3,7 @@ import data from "./actorsData.json"
 import Actor from './Actor'
 import SearchBox from './components/SearchBox'
 
-import { Container, Row, Col ,InputGroup ,FormControl, Form} from 'react-bootstrap';
+import { Container, Row, Col, InputGroup, FormControl, Form,Badge } from 'react-bootstrap';
 
 class Gallery extends Component {
     constructor(props) {
@@ -15,66 +15,82 @@ class Gallery extends Component {
         }
 
         this.searchActors = this.searchActors.bind(this);
-    }  
-        searchActors =(e) => {
+    }
+    searchActors = (e) => {
 
-           var selectedActors=[]
+        var selectedActors = []
 
-         
 
-            
-           
-                this.setState({
-                    
-                    actorSearchResultStrings: e.target.value
-                   
-                })
-            
-    
-        }
-   
+
+
+
+        this.setState({
+
+            actorSearchResultStrings: e.target.value
+
+        })
+
+
+    }
+
     render() {
-        
+
         const { actorSearchResultStrings } = this.state;
-        
-     
+
+
         // filter
-        
+
         let filteredActors = [];
         if (actorSearchResultStrings === "") {
             filteredActors = data;
         } else {
-            for (var i=0;i<data.length;i++){
-               let lowFirst= data[i].FirstName.toLowerCase();
-               let lowsearch = actorSearchResultStrings.toLowerCase();
-            if(lowFirst.startsWith(lowsearch)){
-                filteredActors.push(data[i])
+            var res = actorSearchResultStrings.split(" ");
+
+            for (var i = 0; i < data.length; i++) {
+                let lowFirst = data[i].FirstName.toLowerCase();
+
+                let lowLast = data[i].LastName.toLowerCase();
+                if (res.length === 1) {
+                    let lowsearch = res[0].toLowerCase();
+                    if (lowFirst.startsWith(lowsearch) || lowLast.startsWith(lowsearch)) {
+                        filteredActors.push(data[i])
+                    }
+                } else {
+                    let lowsearchFirst = res[0].toLowerCase();
+                    let lowsearchLast = res[1].toLowerCase();
+                    if (lowFirst === (lowsearchFirst) && lowLast.startsWith(lowsearchLast)) {
+                        filteredActors.push(data[i])
+                    }
+
+                }
             }
         }
-        }
 
 
-        const actorCards = filteredActors.map((actor,index) =>
-        
-          
+        const actorCards = filteredActors.map((actor, index) =>
+
+
             <Col md={4} key={index}>
-                <Actor actor={actor}/>
+                <Actor actor={actor} />
             </Col>
-            
-         
+
+
         )
 
-            
+
 
 
         return (
             <div>
-                
+
                 <Container>
-              {/*  <SearchBox searchPlaceholder="Search Actor" results={actorSearchResultStrings}
+                    {/*  <SearchBox searchPlaceholder="Search Actor" results={actorSearchResultStrings}
                         onSearchChange={this.searchActors}  /> */}
-                <Form.Control type="text" placeholder="Search Actor" value={actorSearchResultStrings} 
-                onChange={this.searchActors}/>
+                    <h1>
+                         <Badge variant="secondary">Actors From IMDB</Badge>
+                    </h1>
+                    <Form.Control className="searchAct" type="text" placeholder="type First Name and/or Last Name to filter " value={actorSearchResultStrings}
+                        onChange={this.searchActors} />
                     <Row>
                         {actorCards}
                     </Row>
