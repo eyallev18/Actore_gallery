48d2d73c67a4c235b81d4a79cdf267ea4612a4d4
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-08 20:55:24 +0200

    Final All Requirments Sets

--- src/Actor.js
+++ src/Actor.js
@@ -5,11 +5,15 @@ export default class Actor extends Component {
     constructor(props) {
         super(props);
         this.actorAge = this.actorAge.bind(this);
+        this.handleClick = this.handleClick.bind(this);
         }
         actorAge(){
             const currentYear = new Date().getFullYear();
             const yearOfBorn = Number(this.props.actor.Birthday.split("-")[0]); 
             return(currentYear-yearOfBorn);
+    }
+    handleClick() {
+      return  ('The link was clicked');
     }
     render() {
         return (
@@ -17,7 +21,8 @@ export default class Actor extends Component {
                 <Card>
                     <Card.Img variant="top" src={this.props.actor.Image}/>
                     <Card.Body>
-                        <Card.Title>{this.props.actor.FirstName} {this.props.actor.LastName} <br/> Age is:  {this.actorAge()}</Card.Title>
+                        <Card.Title ><a href={this.props.actor.IMDBLink} target="_blank"> {this.props.actor.FirstName} {this.props.actor.LastName} </a> </Card.Title>
+                        <Card.Title> Age is:  {this.actorAge()}</Card.Title>
                     </Card.Body>
                 </Card>
             </div>
