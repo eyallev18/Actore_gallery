b435fa6d087f357ba554a187b9e3ca1bc1a28965
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-12 09:29:38 +0200

    Sort is functional by FirstName and LastName Only

--- src/gallery.js
+++ src/gallery.js
@@ -1,99 +1,146 @@
-import React, { Component } from "react"
-import data from "./actorsData.json"
-import Actor from './Actor'
-import SearchBox from './components/SearchBox'
+import React, { Component } from "react";
+import data from "./actorsData.json";
+import Actor from "./Actor";
+import SearchBox from "./components/SearchBox";
 
-import { Container, Row, Col, InputGroup, FormControl, Form,Badge } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Form,
+  Badge,
+  Button,
+  Dropdown
+} from "react-bootstrap";
 
 class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            actorSearchResults: [],
-            actorSearchResultStrings: "",
-            actors: []
+  constructor(props, e) {
+    super(props);
+    this.state = {
+      /*actorSearchResults: [],*/
+      actorSearchResultStrings: "",
+      selectedEventKey: null,
+      value: ""
+      /*actors: [] */
+    };
+
+    this.searchActors = this.searchActors.bind(this);
+    this.sortSelected = this.sortSelected.bind(this);
+  }
+
+  searchActors = e => {
+    this.setState({
+      actorSearchResultStrings: e.target.value
+    });
+  };
+
+  sortSelected(event) {
+    this.setState({ value: event.target.value });
+  }
+  render() {
+    const { actorSearchResultStrings } = this.state;
+    const { value } = this.state;
+    let sortOptions = ["FirstName", "LastName", "Age"];
+
+    // filter
+    if (value === "FirstName" || value === "LastName") {
+      data.sort(function(a, b) {
+        var x = a[value].toLowerCase();
+        var y = b[value].toLowerCase();
+        if (x < y) {
+          return -1;
         }
-
-        this.searchActors = this.searchActors.bind(this);
-        
+        if (x > y) {
+          return 1;
+        }
+        return 0;
+      });
     }
-    
-    searchActors = (e) => {
-        var selectedActors = []
-        this.setState({
+    let filteredActors = [];
+    if (actorSearchResultStrings === "") {
+      filteredActors = data;
+    } else {
+      var res = actorSearchResultStrings.split(" ");
 
-            actorSearchResultStrings: e.target.value
-        })
-    }
+      for (var i = 0; i < data.length; i++) {
+        let lowFirst = data[i].FirstName.toLowerCase();
 
-    render() {
-
-        const { actorSearchResultStrings } = this.state;
-
-
-        // filter
-
-        let filteredActors = [];
-        if (actorSearchResultStrings === "") {
-            filteredActors = data;
+        let lowLast = data[i].LastName.toLowerCase();
+        if (res.length === 1) {
+          let lowsearch = res[0].toLowerCase();
+          if (lowFirst.startsWith(lowsearch) || lowLast.startsWith(lowsearch)) {
+            filteredActors.push(data[i]);
+          }
         } else {
-            var res = actorSearchResultStrings.split(" ");
-
-            for (var i = 0; i < data.length; i++) {
-                let lowFirst = data[i].FirstName.toLowerCase();
-
-                let lowLast = data[i].LastName.toLowerCase();
-                if (res.length === 1) {
-                    let lowsearch = res[0].toLowerCase();
-                    if (lowFirst.startsWith(lowsearch) || lowLast.startsWith(lowsearch)) {
-                        filteredActors.push(data[i])
-                    }
-                } else {
-                    let lowsearchFirst = res[0].toLowerCase();
-                    let lowsearchLast = res[1].toLowerCase();
-                    if (lowFirst === (lowsearchFirst) && lowLast.startsWith(lowsearchLast)) {
-                        filteredActors.push(data[i])
-                    }
-
-                }
-            }
+          let lowsearchFirst = res[0].toLowerCase();
+          let lowsearchLast = res[1].toLowerCase();
+          if (
+            lowFirst === lowsearchFirst &&
+            lowLast.startsWith(lowsearchLast)
+          ) {
+            filteredActors.push(data[i]);
+          }
         }
+      }
+    } /*else if (value === "Age"){
+      for (let i=0;i<filteredActors.length;i++){
+        for (let j=0;j<i;j++){
+          if (this.actor.actorAge()
+        }
+      }
 
 
-        const actorCards = filteredActors.map((actor, index) =>
+    }*/
+    /* if (value === "") {
+      var unSortedArray = [];
+      for (let i = 0; i < filteredActors.length; i++) {
+        unSortedArray.push(filteredActors[i]);
+      }
+    } */
+    /*if (value === "" || value === "unSorted") {
+    } else  */ /*else if (value === "notSorted") {
+      filteredActors = [];
+      for (let i = 0; i < unSortedArray.length; i++) {
+        filteredActors.push(unSortedArray[i]);
+      }
+    } */
+    const actorCards = filteredActors.map((actor, index) => (
+      <Col md={4} key={index}>
+        <Actor actor={actor} />
+      </Col>
+    ));
 
-
-            <Col md={4} key={index}>
-                <Actor actor={actor} />
-            </Col>
-
-
-        )
-
-
-
-
-        return (
-            <div>
-
-                <Container>
-                    {/*  <SearchBox searchPlaceholder="Search Actor" results={actorSearchResultStrings}
+    return (
+      <div>
+        <Container>
+          {/*  <SearchBox searchPlaceholder="Search Actor" results={actorSearchResultStrings}
                         onSearchChange={this.searchActors}  /> */}
-                    <h1>
-                         <Badge variant="secondary">Actors From IMDB</Badge>
-                    </h1>
-                    <Form.Control className="searchAct" type="text" placeholder="type First Name and/or Last Name to filter " value={actorSearchResultStrings}
-                        onChange={this.searchActors} />
-                    <Row>
-                        {actorCards}
-                    </Row>
-                </Container>
-
-
-            </div>
-
-        )
-    }
+          <h1>
+            <Badge variant="secondary">Actors From IMDB</Badge>
+          </h1>
+          <select value={value} onChange={this.sortSelected}>
+            <option value="unSorted">unSorted</option>
+            <option value="FirstName">Sorted By FirstName</option>
+            <option value="LastName">Sorted By LastName</option>
+            <option value="Age">Sorted By Age</option>
+          </select>
+          {/* <Button variant="success" OnClick={this.sortActors}>
+            Sort
+                    </Button> */}
+          <Form.Control
+            className="searchAct"
+            type="text"
+            placeholder="type First Name and/or Last Name to filter "
+            value={actorSearchResultStrings}
+            onChange={this.searchActors}
+          />
+          <Row>{actorCards}</Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Gallery;
