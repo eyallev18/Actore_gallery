fd205f78ed7f29cefaea80a5a153755a0c5ffadd
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-14 11:50:12 +0200

    Updates

---
+++ precache-manifest.9ce07c0ebf1e50a034d7e2ac76fb606d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bf9424ac8d720fb980db24d235694f",
+    "url": "/Actore_gallery/index.html"
+  },
+  {
+    "revision": "318d4c489b464cf51203",
+    "url": "/Actore_gallery/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a5ddd67dccfc6495ae87",
+    "url": "/Actore_gallery/static/css/main.3f2c7274.chunk.css"
+  },
+  {
+    "revision": "318d4c489b464cf51203",
+    "url": "/Actore_gallery/static/js/2.e846711f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Actore_gallery/static/js/2.e846711f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5ddd67dccfc6495ae87",
+    "url": "/Actore_gallery/static/js/main.794a35dd.chunk.js"
+  },
+  {
+    "revision": "ae89d6234866be593e66",
+    "url": "/Actore_gallery/static/js/runtime-main.9bea5660.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Actore_gallery/static/media/logo.ee7cd8ed.svg"
+  }
+]);
